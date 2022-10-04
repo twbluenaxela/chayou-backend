cead98043ee57f20e5681e3bc0870a6ffae3ea0a
于樂洋 <rednaxela64@outlook.com>
2022-10-04 15:47:00 -0700

    hook up backend to mongodb

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const mongoose = require('mongoose')
-const cors = require('cors')
-const express = require('express')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
-const teaRouter = require('./controllers/tea')
-const middleware = require('./utils/middleware')
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const teaRouter = require("./controllers/tea");
+const teaCategoryRouter = require("./controllers/teaCategory");
+const middleware = require("./utils/middleware");
 
 const app = express();
 
-logger.info(`Connecting to ${config.MONGODB_URI}`)
+logger.info(`Connecting to ${config.MONGODB_URI}`);
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB", error.message);
+  });
 
-app.use('/api/tea', teaRouter)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/tea", teaRouter);
+app.use("/api/category", teaCategoryRouter);
 
 // if(process.env.NODE_ENV === 'testing'){
 
 // }
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ controllers/teaCategory.js
@@ -0,0 +1,47 @@
+const teaCategoryRouter = require('express').Router()
+const { response } = require('express')
+const TeaCategory = require('../models/teaCategory')
+const logger = require('../utils/logger')
+const middleware = require('../utils/middleware')
+
+teaCategoryRouter.get('/', async (req, res) => {
+    logger.info('Grabbing tea categories...')
+    const teaCategoryList = await TeaCategory.find({})
+    logger.info(`Tea category list: ${teaCategoryList}`)
+    res.json(teaCategoryList)
+
+})
+
+teaCategoryRouter.get('/:type', async (req, res) => {
+    const teaCategoryToSearchFor = req.params['type']
+    const matchedTeaCategories = await TeaCategory.find({ type: teaCategoryToSearchFor })
+
+    if(matchedTeaCategories){
+        res.json(matchedTeaCategories)
+    }else{
+        res.status(404).end()
+    }
+})
+
+teaCategoryRouter.post('/', async (req, res) => {
+    const receivedTeaCategory = req.body
+    console.log('Received: ', receivedTeaCategory)
+
+    const category = new TeaCategory({
+        englishName: receivedTeaCategory.englishName,
+        chineseName: receivedTeaCategory.chineseName,
+        type: receivedTeaCategory.type
+    })
+
+    if(category.englishName && category.chineseName && category.type){
+        const savedTeaCategory = await category.save()
+        res.status(201).json(savedTeaCategory)
+
+    }else{
+        res.sendStatus(400)
+    }
+})
+
+
+
+module.exports = teaCategoryRouter;
